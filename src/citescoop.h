366966bf60324fb49e8c4d57aa09210a5679c832
Zoe Nickson <mjn6@st-andrews.ac.uk>
2025-10-21 18:17:27 +0100

    Removed the files generated by cmake init
    Signed-off-by: Zoe Nickson <mjn6@st-andrews.ac.uk>

--- src/citescoop.h
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: 2025 The University of St Andrews
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-#ifndef SRC_CITESCOOP_H_
-#define SRC_CITESCOOP_H_
-#endif  // SRC_CITESCOOP_H_
