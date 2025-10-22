caf4b29f33c8cd75fdeae6f89fbb224fca25c8f1
Zoe Nickson <mjn6@st-andrews.ac.uk>
2025-10-23 00:22:04 +0100

    Placeholder parsing methods
    Signed-off-by: Zoe Nickson <mjn6@st-andrews.ac.uk>

--- src/extract/dump_parser.cc
+++ src/extract/dump_parser.cc
@@ -27,12 +27,56 @@ std::unique_ptr<std::vector<proto::Page>> DumpParser::ParseXML(
     stream.read(buf, buf_size - 1);
     if (stream.gcount()) {
       xmlpp::ustring input(buf, buf + stream.gcount());
+      parse_chunk(input);
     }
   } while (stream);
 
   finish_chunk_parsing();
 
   return pages;
+}
+
+void DumpParser::on_start_document() {
+  // std::cout << "on_start_document()" << std::endl;
+}
+
+void DumpParser::on_end_document() {
+  // std::cout << "on_end_document()" << std::endl;
+}
+
+void DumpParser::on_start_element(const xmlpp::ustring& name,
+                                  const AttributeList& attributes) {
+  // std::cout << "node name=" << name << std::endl;
+
+  // // Print attributes:
+  // for (const auto& attr_pair : attributes) {
+  //   std::cout << "  Attribute name=" << attr_pair.name << std::endl;
+  //   std::cout << "    , value= " << attr_pair.value << std::endl;
+  // }
+}
+
+void DumpParser::on_end_element(const xmlpp::ustring& /* name */) {
+  // std::cout << "on_end_element()" << std::endl;
+}
+
+void DumpParser::on_characters(const xmlpp::ustring& text) {
+  // std::cout << "on_characters(): " << text << std::endl;
+}
+
+void DumpParser::on_comment(const xmlpp::ustring& text) {
+  // std::cout << "on_comment(): " << text << std::endl;
+}
+
+void DumpParser::on_warning(const xmlpp::ustring& text) {
+  // std::cout << "on_warning(): " << text << std::endl;
+}
+
+void DumpParser::on_error(const xmlpp::ustring& text) {
+  // std::cout << "on_error(): " << text << std::endl;
+}
+
+void DumpParser::on_fatal_error(const xmlpp::ustring& text) {
+  // std::cout << "on_fatal_error(): " << text << std::endl;
 }
 
 }  // namespace wikiopencite::citescoop
