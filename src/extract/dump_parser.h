515ae6dab7ea6f4f9236da28e39ff0182f838aa1
Zoe Nickson <mjn6@st-andrews.ac.uk>
2025-11-20 21:34:06 +0000

    Refactored DumpParser for clarity and inheritance
    DumpParser has been refactored to make it more readable (it wasn't exactly pretty before). Also because the stream parser will need to inherit from it, it needed to be made more suitable for inheritance.
    
    To inherit from DumpParser, the inheriting class should override the Store method to provide it's own way of storing the produced page and revisions. Once Store is called for a given page, the DumpParser has finished processing them so the inheriting class is free to do as it wishes with it.
    
    Signed-off-by: Zoe Nickson <mjn6@st-andrews.ac.uk>

--- src/extract/dump_parser.h
+++ src/extract/dump_parser.h
@@ -39,8 +39,8 @@ class DumpParser : public xmlpp::SaxParser {
 
  protected:
   // SAX parser overrides
-  void on_start_document() override;
-  void on_end_document() override;
+  // void on_start_document() override;
+  // void on_end_document() override;
   void on_start_element(const xmlpp::ustring& name,
                         const AttributeList& properties) override;
   void on_end_element(const xmlpp::ustring& name) override;
@@ -48,6 +48,19 @@ class DumpParser : public xmlpp::SaxParser {
   void on_warning(const xmlpp::ustring& text) override;
   void on_error(const xmlpp::ustring& text) override;
   void on_fatal_error(const xmlpp::ustring& text) override;
+
+  /// @brief Store a page and it's referenced revisions.
+  /// Once the parser calls this method for a given page, it has
+  /// finished with the page so you may do with it as you wish.
+  /// @param revisions Revisions to store.
+  /// @param page Page to store.
+  virtual void Store(
+      const std::map<uint64_t, wikiopencite::proto::Revision>& revisions,
+      const wikiopencite::proto::Page& page);
+
+  /// @brief Start parsing the input stream.
+  /// @param stream Input stream to parse.
+  void StartParser(std::istream& stream);
 
  private:
   std::shared_ptr<wikiopencite::citescoop::Parser> parser_;
@@ -60,20 +73,82 @@ class DumpParser : public xmlpp::SaxParser {
   bool store_revision_;
 
   std::string text_buf_;
-  std::vector<wikiopencite::proto::RevisionCitations> all_page_citations_;
-  wikiopencite::proto::RevisionCitations revision_citations_;
+  std::vector<wikiopencite::proto::RevisionCitations> citations_by_revision_;
+  wikiopencite::proto::RevisionCitations current_citations_;
   wikiopencite::proto::Page current_page_;
   wikiopencite::proto::Revision current_revision_;
-  std::map<int64_t, wikiopencite::proto::Revision> page_revisions_;
+  std::map<uint64_t, wikiopencite::proto::Revision> current_page_revisions_;
+  std::map<uint64_t, wikiopencite::proto::Revision> revisions_to_store_;
 
-  std::unique_ptr<std::vector<wikiopencite::proto::Page>> pages_;
-  std::unique_ptr<std::map<uint64_t, wikiopencite::proto::Revision>> revisions_;
+  std::unique_ptr<std::vector<wikiopencite::proto::Page>> stored_pages_;
+  std::unique_ptr<std::map<uint64_t, wikiopencite::proto::Revision>>
+      stored_revisions_;
 
   /// @brief Complete the pages citations.
   ///
   /// Will deduplicate the citations and make sure only the first and
   /// last revision is referenced by the citation.
   void MakePageCitationList();
+
+  /// @brief Reset the parser state.
+  void ResetState();
+
+  /// @brief Handle when a field ends.
+  /// Handles fields such as id, text and so on.
+  /// @param field_name Name of the field that has ended.
+  void OnEndField(const xmlpp::ustring& field_name);
+
+  /// @brief Handle the end of a page.
+  /// Will assemble the deduplicated page citations list, store the
+  /// current page and then clear any datastructures used in page
+  /// processing ready for the next page.
+  void OnEndPage();
+
+  /// @brief Handle the end of a revision.
+  /// Adds the current revision and it's citations to the required
+  /// datastructures and clears it ready for the next one.
+  void OnEndRevision();
+
+  /// @brief Initialize the required datastructures for the parser.
+  void InitializeParser();
+
+  /// @brief Check if any citations already discovered exist in this
+  /// revision.
+  /// Any citations that are not found in this revision and that have
+  /// not already been marked as removed will be marked as removed in
+  /// this revision. Any citations that have been previously marked as
+  /// removed but have been re-added in this revision will be marked as
+  /// present again. Note: In this case, the revision_added field will
+  /// still show the revision in which the citation was originally
+  /// added, the fact it was removed and re-added will not be stored and
+  /// this information is lost.
+  ///
+  /// @param citations Citations in this revision.
+  /// @param discovered_citations Map of citations previously discovered
+  /// for this page.
+  /// @param ref_count Map of the number of references for each
+  /// revision. Used to make sure only revisions with references are
+  /// stored.
+  void CheckExistingCitations(
+      wikiopencite::proto::RevisionCitations* citations,
+      std::map<std::string, wikiopencite::proto::Citation>*
+          discovered_citations,
+      std::map<uint64_t, int>* ref_count);
+
+  /// @brief Add any citations not already discovered.
+  /// For any citations that do not already exist in
+  /// discovered_citations, this add them setting the revision_added
+  /// field to the current revision ID.
+  /// @param citations Citations in this revision.
+  /// @param discovered_citations Map of citations already found for
+  /// this page.
+  /// @param ref_count Map of the number of references for each
+  /// revision. Used to make sure only revisions with references are
+  /// stored.
+  void AddNewCitations(wikiopencite::proto::RevisionCitations* citations,
+                       std::map<std::string, wikiopencite::proto::Citation>*
+                           discovered_citations,
+                       std::map<uint64_t, int>* ref_count);
 };
 }  // namespace wikiopencite::citescoop
 
